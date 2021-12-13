584a09155dd14d4ddfffa18e7926bffeb89f3e8d
F3FFO <federico.pieranton@gmail.com>
2021-12-13 20:59:05 +0100

    Added panel to JFrame

--- src/main/java/nexCell/Main.java
+++ src/main/java/nexCell/Main.java
@@ -1,8 +1,10 @@
 package nexCell;
 
+import nexCell.view.Gui;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        new Gui();
     }
 }
---
+++ src/main/java/nexCell/view/Gui.java
@@ -0,0 +1,32 @@
+package nexCell.view;
+
+import nexCell.view.customElement.SheetsView;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui extends JFrame {
+
+    private final JPanel PANELTOP = new JPanel();
+    private final SheetsView PANELMID = new SheetsView();
+    private final JPanel PANELBOTTOM = new JPanel();
+
+    private static final JScrollPane SCROLLPANE = new JScrollPane();
+
+    public Gui() {
+        super("NexCell");
+        this.setLayout(new BorderLayout());
+        this.setExtendedState(Frame.MAXIMIZED_BOTH);
+
+        SCROLLPANE.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        SCROLLPANE.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        SCROLLPANE.getViewport().add(PANELMID);
+        this.add(SCROLLPANE, BorderLayout.CENTER);
+
+        this.setResizable(true);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
