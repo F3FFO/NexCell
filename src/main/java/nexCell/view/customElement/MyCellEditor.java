c54d6c6dcc9432a5d94d23c0cf99a90f1aa20cd1
F3FFO <federico.pieranton@gmail.com>
2021-12-23 10:51:21 +0100

    Optimize memory usage

--- src/main/java/nexCell/view/customElement/MyCellEditor.java
+++ src/main/java/nexCell/view/customElement/MyCellEditor.java
@@ -24,17 +24,8 @@ public class MyCellEditor extends DefaultCellEditor {
     @Override
     public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
         if (sheetStructure.getMatrix().get(row).get(column) instanceof CellFormula) {
-            String formula;
-            int i;
-            for (i = 0; i < sheetStructure.getCellFormula().size(); i++) {
-                int rowFormula = sheetStructure.getCellFormula().get(i).getRow();
-                int columnFormula = sheetStructure.getCellFormula().get(i).getColumn();
-                if (rowFormula == row && columnFormula == column) {
-                    formula = sheetStructure.getCellFormula().get(i).getOriginalValue();
-                    return super.getTableCellEditorComponent(table, formula, isSelected, row, column);
-                }
-            }
-            return super.getTableCellEditorComponent(table, null, isSelected, row, column);
+            String formula = ((CellFormula) sheetStructure.getMatrix().get(row).get(column)).getOriginalValue();
+            return super.getTableCellEditorComponent(table, formula, isSelected, row, column);
         } else
             return super.getTableCellEditorComponent(table, value, isSelected, row, column);
     }
