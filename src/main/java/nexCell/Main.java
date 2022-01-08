3ec3c19faddadaa9b4d66261ea8b206f38c538a1
F3FFO <federico.pieranton@gmail.com>
2022-01-08 11:06:32 +0100

    initial commit

--- src/main/java/nexCell/Main.java
+++ src/main/java/nexCell/Main.java
@@ -4,12 +4,21 @@ import com.formdev.flatlaf.FlatLightLaf;
 import nexCell.view.Gui;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FlatLightLaf.setup();
         UIManager.put("TextComponent.arc", 30);
-        new Gui();
+        Gui g = new Gui();
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        Runnable save = g.saveTemp(File.createTempFile(".unsaved", ".tmp"));
+        executor.scheduleAtFixedRate(save, 10, 15, TimeUnit.SECONDS);
     }
 }
