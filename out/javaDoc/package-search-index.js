31aa07fba1f348e699ccd7458dd271f7a412af18
F3FFO <federico.pieranton@gmail.com>
2022-01-23 17:42:10 +0100

    add javadoc files

---
+++ out/javaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"nexCell"},{"l":"nexCell.cell"},{"l":"nexCell.controller"},{"l":"nexCell.controller.io"},{"l":"nexCell.view"},{"l":"nexCell.view.menu"},{"l":"nexCell.view.panel"},{"l":"nexCell.view.rowHeader"}]
