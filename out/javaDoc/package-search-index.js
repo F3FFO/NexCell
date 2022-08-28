39b128b8c4731c720d9f2b75d2e921f03eb7a19c
Federico Pierantoni <38475841+F3FFO@users.noreply.github.com>
2022-08-28 15:55:57 +0200

    Delete out/javaDoc directory

--- out/javaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"nexCell"},{"l":"nexCell.cell"},{"l":"nexCell.controller"},{"l":"nexCell.controller.io"},{"l":"nexCell.view"},{"l":"nexCell.view.menu"},{"l":"nexCell.view.panel"},{"l":"nexCell.view.rowHeader"}]
